ad9079914015c9317623a39ea0ab9678ee55e345
Hernan <hcuffaro@gmail.com>
2020-08-20 18:11:53 -0300

    Adding JavaDocs

--- src/main/java/com/agile/engine/cuffaro/model/strategies/OperationStrategy.java
+++ src/main/java/com/agile/engine/cuffaro/model/strategies/OperationStrategy.java
@@ -2,6 +2,18 @@ package com.agile.engine.cuffaro.model.strategies;
 
 import java.math.BigDecimal;
 
+/**
+ * Methods that should implement the transactions.
+ * 
+ * @author hcuff
+ *
+ */
+
 public interface OperationStrategy {
+	/**
+	 * Defines the way that the transaction modifies the balance amount.
+	 * @param otherValue
+	 * @return
+	 */
 	public BigDecimal doOperation(BigDecimal otherValue);
 }
--- src/main/java/com/agile/engine/cuffaro/model/strategies/TransactionFactory.java
+++ src/main/java/com/agile/engine/cuffaro/model/strategies/TransactionFactory.java
@@ -8,17 +8,35 @@ import com.agile.engine.cuffaro.dto.operations.TransactionDebitDTO;
 import com.agile.engine.cuffaro.enums.TransactionTypeEnum;
 import com.agile.engine.cuffaro.exceptions.InvalidArgumentException;
 import com.agile.engine.cuffaro.model.TransactionItem;
-
+/**
+ * Factory class that returns TransactionsDTO based on the transaction type.
+ * See different transaction types in TransactionTypeEnum
+ * @author hcuff
+ *
+ */
 public final class TransactionFactory {
 
 	private TransactionFactory() {}
 	
+	/**
+	 * Create and populate the amount of a transactionDTO
+	 * @param type
+	 * @param amount
+	 * @return
+	 * @throws InvalidArgumentException
+	 */
 	public static TransactionDTO createTransactionDTO(TransactionTypeEnum type, BigDecimal amount) throws InvalidArgumentException {
 		TransactionDTO dto = createTransactionDTO(type);
 		dto.setAmount(amount);
 		return dto;
 	}
 	
+	/**
+	 * Create and populate a transactionDTO based on a transactionItem
+	 * @param item
+	 * @return
+	 * @throws InvalidArgumentException
+	 */
 	public static TransactionDTO createTransactionDTO(TransactionItem item) throws InvalidArgumentException {
 		TransactionDTO dto = createTransactionDTO(item.getTransactionType());
 		dto.setAmount(item.getAmount());
@@ -27,6 +45,12 @@ public final class TransactionFactory {
 		return dto;
 	}
 	
+	/**
+	 * Creates a TransactionDTO based on a transactionTypeEnum.
+	 * @param type
+	 * @return
+	 * @throws InvalidArgumentException
+	 */
 	public static TransactionDTO createTransactionDTO(TransactionTypeEnum type) throws InvalidArgumentException {
 		switch (type) {
 		case credit:
@@ -36,6 +60,6 @@ public final class TransactionFactory {
 		default:
 			break;
 		}
-		throw new InvalidArgumentException("Invalid Type operation");
+		throw new InvalidArgumentException("Invalid Operation Type");
 	}
 }
